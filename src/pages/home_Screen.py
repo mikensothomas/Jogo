7f9cd6c39ed2d49d68aaeda2d8d54f0e24e23429
Mikensonthomas142 <mikensonthomas2@gmail.com>
2026-03-30 23:21:57 -0300

    Menu funcionando

--- src/pages/home_Screen.py
+++ src/pages/home_Screen.py
@@ -57,17 +57,19 @@ count_ball = 0
 paused = False
 started = False
 finished = False
+continueGame = False
 
 font = pg.font.SysFont("Arial", 60)
-font_menu = pg.font.SysFont("Arial", 20)
+font_menu = pg.font.SysFont("Arial", 25)
 font_score = pg.font.SysFont("Arial", 30)
 title = font.render("REBELIÃO DAS BOLINHAS", True, colors['Laranja'])
-paused_game = font.render("JOGO PAUSADO", True, colors['Vermelho'])
+paused_game = font.render("JOGO PAUSADO", True, colors['Amarelo'])
 pg.display.set_caption('REBELIÃO DAS BOLINHAS')
 
 start = font_menu.render("I: Iniciar", True, colors['Verde'])
 pause = font_menu.render("P: Pausar", True, colors['Amarelo'])
 finish = font_menu.render("T: Terminar", True, colors['Vermelho'])
+continue_game = font_menu.render("C: Continuar", True, colors['Laranja'])
 
 running = True
 
@@ -95,23 +97,34 @@ while running:
                 dy = -bullet_speed * math.sin(rad)
 
                 bullets.append(BulletGame(screen, colors['Vermelho'], position_x + 100, position_y, dx, dy, ray))
-                shoot_sound.play(maxtime=50)
+
+                if not paused and started:
+                    shoot_sound.play(maxtime=50)
+
             if event.key == pg.K_p:
-                paused = not paused
+                paused = True
+            if event.key == pg.K_i:
+                started = True
+            if event.key == pg.K_c:
+                if paused == True:
+                    paused = False
+            if event.key == pg.K_t:
+                running = False
 
     keyboard = pg.key.get_pressed()
     pontuation = font_score.render(f"Pontuação: {score} ", True, colors['Laranja'])
     count = font_score.render(f"Acertou: {count_ball} em {ball_game_count} ", True, colors['Laranja'])
 
     if not paused:
-        if keyboard[pg.K_RIGHT]:
-            current_image = rotation_image
-            position_x += speed
-            move_sound.play(maxtime=50)
-        if keyboard[pg.K_LEFT]:
-            current_image = pg.transform.flip(rotation_image, True, False)
-            position_x -= speed
-            move_sound.play(maxtime=50)
+        if started:
+            if keyboard[pg.K_RIGHT]:
+                current_image = rotation_image
+                position_x += speed
+                move_sound.play(maxtime=50)
+            if keyboard[pg.K_LEFT]:
+                current_image = pg.transform.flip(rotation_image, True, False)
+                position_x -= speed
+                move_sound.play(maxtime=50)
 
     if position_x < 0:
         position_x = 0
@@ -136,40 +149,42 @@ while running:
 
     if paused:
         screen.blit(paused_game, (360, 300))
+        screen.blit(continue_game, (300, 10))
         
     screen.blit(title, (250, 50))
     screen.blit(pontuation, (950, 550))
     screen.blit(count, (950, 650))
     screen.blit(start, (10, 10))
-    screen.blit(pause, (80, 10))
-    screen.blit(finish, (165, 10))
+    screen.blit(pause, (90, 10))
+    screen.blit(finish, (190, 10))
     
     if not paused:
-        if current_time - last_time > interval:
-            balls.append(BallGame(screen, colors['RosaClaro'], (width, random.randint(0, height-350)), 8))
-            last_time = current_time
-            ball_game_count += 1
+        if started:
+            if current_time - last_time > interval:
+                balls.append(BallGame(screen, colors['RosaClaro'], (width, random.randint(0, height-350)), 8))
+                last_time = current_time
+                ball_game_count += 1
 
-        for ball in balls:
-            ball.move_balls(speed_balls)
-            ball.draw_balls()
+            for ball in balls:
+                ball.move_balls(speed_balls)
+                ball.draw_balls()
 
-        for bullet in bullets[:]:
-            bullet.move_bullet()
-            bullet.draw_bullet()
+            for bullet in bullets[:]:
+                bullet.move_bullet()
+                bullet.draw_bullet()
 
-            if bullet.is_off_screen(width, height):
-                bullets.remove(bullet)
-
-        for bullet in bullets[:]:
-            for ball in balls[:]:
-                if bullet.collide(ball):
-                    balls.remove(ball)
-                    count_ball += 1
+                if bullet.is_off_screen(width, height):
                     bullets.remove(bullet)
-                    score += 10
-                    collision_sound.play()
-                    break
+
+            for bullet in bullets[:]:
+                for ball in balls[:]:
+                    if bullet.collide(ball):
+                        balls.remove(ball)
+                        count_ball += 1
+                        bullets.remove(bullet)
+                        score += 10
+                        collision_sound.play()
+                        break
                 
     pg.display.flip()
 
